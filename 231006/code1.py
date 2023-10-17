c085a1a9e9fde4381462a7c6adae6ce6cf4cd043
offpython <offpython@gmail.com>
2023-10-17 15:33:39 +0900

    Preprocessing

--- 231006/code1.py
+++ 231006/code1.py
@@ -4,10 +4,11 @@ import matplotlib.pyplot as plt
 def get_y(x): return (1/10) * x**2
 def get_dy_dx(x): return (1/5) * x
 
-x = 3; y = get_y(x)
-
 function_x = np.linspace(-5, 5, 100)
 function_y = (1/10) * function_x**2
+
+x = 3; y = get_y(x)
+x = x - get_dy_dx(x)
 
 fig, ax = plt.subplots(figsize=(10, 5))
 ax.plot(function_x, function_y)
